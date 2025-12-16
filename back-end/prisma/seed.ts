15fc635bb4c0a382403fb0ee65551bc03691adf7
Mauro Leal <mauro.costa.12.j@hotmail.com>
2025-12-16 09:54:39 -0300

    feat: seed

--- back-end/prisma/seed.ts
+++ back-end/prisma/seed.ts
@@ -1,10 +1,50 @@
-import { prisma } from "../prisma.js"
+import bcrypt from 'bcryptjs';
+import { prisma } from '../prisma.js';
+import { PlanType, UserRole } from '../src/generated/prisma/client.js';
 
-const seed = async ()=>{
-  await prisma.user.create({
+const seed = async () => {
+  const password = process.env.ADMIN_PASSWORD!;
+  if (!password) {
+    throw new Error('ADMIN_PASSWORD is not set');
+  }
+  const passwordHash = await bcrypt.hashSync(password, 10);
+
+  const adminEmail = process.env.ADMIN_EMAIL!;
+  if (!adminEmail) {
+    throw new Error('ADMIN_EMAIL is not set');
+  }
+
+  const adminExists = await prisma.user.findUnique({
+    where: {
+      email: adminEmail,
+    },
+  });
+
+  if (adminExists) {
+    console.log('Admin already exists');
+    return;
+  }
+
+  const admin = await prisma.user.create({
     data: {
-      name: 'John Doe',
-      email: 'L2b0M@example.com',
-      // role: 'ADMIN'
-  })
-}
+      name: 'Admin',
+      email: adminEmail,
+      passwordHash,
+      role: UserRole.ADMIN,
+    },
+  });
+
+  await prisma.subscription.create({
+    data: {
+      userId: admin.id,
+      plan: PlanType.ENTERPRISE,
+      expiresAt: new Date(new Date().setFullYear(new Date().getFullYear() + 100)),
+    },
+  });
+  console.log(`Admin created with id ${admin.id}`);
+  return;
+};
+
+seed()
+  .catch(console.error)
+  .finally(() => prisma.$disconnect());
